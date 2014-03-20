24e2cc5fea5ef44d5a68d0a372926135a515d5ca
Igor Shevchenko <igoro.shevchenko@gmail.com>
2014-03-20 22:41:17 +0100

    Search Field aka 'Google'.

--- src/main/java/it4bi/ufrt/ir/service/users/UsersService.java
+++ src/main/java/it4bi/ufrt/ir/service/users/UsersService.java
@@ -14,9 +14,9 @@ public class UsersService {
 	public List<User> getUsers() {
 		
 		User[] users = new User[5];
-		users[0] = new User(0, "Igor", "Shevchenko", "Ukraine", UserSex.MALE, "1995-05-28");
-		users[1] = new User(1, "Copy", "Shevchenko", "Ukraine", UserSex.MALE, "1995-05-28");
-		users[2] = new User(2, "Peter", "Markovich", "France", UserSex.MALE, "1982-07-14");
+		users[0] = new User(0, "Richard", "Kondor", "Italy", UserSex.MALE, "1995-05-28");
+		users[1] = new User(1, "Jack", "Willis", "Greece", UserSex.MALE, "1995-05-28");
+		users[2] = new User(2, "Peter", "Brando", "France", UserSex.MALE, "1982-07-14");
 		users[3] = new User(3, "Lara", "Kronch", "Spain", UserSex.FEMALE, "1992-11-16");
 		users[4] = new User(4, "Anna", "Micht", "Germany", UserSex.FEMALE, "1989-03-18");
 		
--- src/main/webapp/JS/SearchEngine.js
+++ src/main/webapp/JS/SearchEngine.js
@@ -3,14 +3,15 @@
 function searchEngineViewModel() {
 
     var self = {};
-    self.description = "Hello, I am Brain";
-
+    
+    self.mode = ko.observable('Search');
     self.users = ko.observableArray([]);
     self.loggedUser = ko.observable();
+    self.searchQuery = ko.observable('');
     self.resultsDOC = ko.observableArray([]);
 
     self.initialize = function () {
-        // Get list of rigestered users
+        // Get list of registered users
         dataServiceProvider.getUsers(function (users) {
             // Need to insert objects into 'ko.observableArray' and not to substitute the array
             $.each(users, function (i, u) {
@@ -19,14 +20,35 @@ function searchEngineViewModel() {
         });
     };
 
+    // Show profile of logged-in user
+    self.showMyProfile = function () {
+        self.mode('MyProfile');
+    }
+
+    // Show documents uploaded by logged-in user
+    self.showMyFiles = function () {
+        self.mode('MyFiles');
+    }
+
+    // Show page to upload document by logged-in user
+    self.showUploadFile = function () {
+        self.mode('UploadFile');
+    }
+
+    // Show search page
+    self.showSearchPage = function () {
+        self.mode('Search');
+    }
+    
     // Search by EVERYTHING
     self.performSearch = function () {
         userID = self.loggedUser().id;
+        query = self.searchQuery();
 
-        self.searchDOC("SOME QUERY", userID);
+        self.searchDOC(query, userID);
     };
 
-    // Search by DOCUMENTS for given user
+    // Search by DOCUMENTS by given user
     self.searchDOC = function (query, userID) {
         dataServiceProvider.searchDOC(query, userID, function (documents) {
             self.resultsDOC.removeAll();
