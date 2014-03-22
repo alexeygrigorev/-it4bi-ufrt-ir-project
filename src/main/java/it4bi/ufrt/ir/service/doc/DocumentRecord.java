8a706e4e038a8095e7f8ef751ac86b86dab325d5
Ahmet Anıl Pala <aanilpala@gmail.com>
2014-03-22 12:51:29 +0100

    Baseline implementation for document indexing and interface for DB connections. Not tested! Just to give you an idea how documents service grows.

--- src/main/java/it4bi/ufrt/ir/service/doc/DocumentRecord.java
+++ src/main/java/it4bi/ufrt/ir/service/doc/DocumentRecord.java
@@ -1,41 +1,97 @@
 package it4bi.ufrt.ir.service.doc;
 
+import java.io.IOException;
+import java.util.Vector;
+
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
 public class DocumentRecord {
 
-	private String docName;
+	private String docTitle;
 	private String docPath;
-	private String docId;
+	private static int docId = 100000;
 
 	public DocumentRecord() {
 	}
 
-	public DocumentRecord(String docName, String docPath) {
-		this.docName = docName;
+	public String getDocTitle() {
+		return docTitle;
+	}
+	
+	public DocumentRecord(String docTitle) {
+		this.docId = docId++;
+		this.docTitle = docTitle;
+	}
+	
+	public DocumentRecord(String docTitle, String docPath) {
+		this.docId = docId++;
 		this.docPath = docPath;
+		this.docTitle = docTitle;
 	}
-
-	public String getDocName() {
-		return docName;
+	
+	public String getFullText() {
+		String docText = DocumentReader.readDoc(docPath);
+		return docText;
 	}
-
-	public String getDocId() {
-		return docId;
+	
+	public void indexDocument() throws Exception {  // Custom Exception Classes can be introduced
+		
+		if(docPath == null) throw new Exception();
+		
+		// configure index properties
+        EnglishAnalyzer analyzer = new EnglishAnalyzer(Version.LUCENE_41);  
+      	Directory indexDir = new RAMDirectory();
+      	
+      	
+        DocumentIndexer indexer = new DocumentIndexer(indexDir, analyzer);
+        
+        indexer.indexDocument(this);
+		
 	}
+	
+	public static void rebuildDocsIndex() throws Exception {
 
-	public void setDocId(String docId) {
-		this.docId = docId;
+		// configure index properties
+        EnglishAnalyzer analyzer = new EnglishAnalyzer(Version.LUCENE_41);  
+      	Directory indexDir = new RAMDirectory();
+      	
+        DocumentIndexer indexer = new DocumentIndexer(indexDir, analyzer);
+        
+        indexer.deleteIndex();
+        
+        Vector<DocumentRecord> allDocumentRecords = DocumentsDAO.getAllDocuments();
+        
+        for(DocumentRecord documentRecord : allDocumentRecords) {
+			documentRecord.setDocPath();
+			documentRecord.indexDocument();
+		}
+        
+        
 	}
-
-	public void setDocName(String docName) {
-		this.docName = docName;
+	
+	public void setDocTitle(String docTitle) {
+		this.docTitle = docTitle;
 	}
 
 	public String getDocPath() {
 		return docPath;
 	}
 
-	public void setDocPath(String docPath) {
-		this.docPath = docPath;
+	public void setDocPath() {
+		this.docPath = DocumentsDAO.getDocPath(this.docId);
 	}
+
+	public int getDocId() {
+		return docId;
+	}
+
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+
+	
 
 }
