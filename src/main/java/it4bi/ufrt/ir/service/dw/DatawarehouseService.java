3b13081664a2cad68234e9bb32594773d24c465b
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2014-04-11 15:02:11 +0200

    now it should compile

--- src/main/java/it4bi/ufrt/ir/service/dw/DatawarehouseService.java
+++ src/main/java/it4bi/ufrt/ir/service/dw/DatawarehouseService.java
@@ -1,16 +1,10 @@
 package it4bi.ufrt.ir.service.dw;
 
-import it4bi.ufrt.ir.service.dw.evaluation.QueryTemplateConfigurationDao;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatawarehouseService {
 
-	@Autowired
-	private QueryTemplateConfigurationDao configurationDao;
-	
 	// stuff
 	
 }
