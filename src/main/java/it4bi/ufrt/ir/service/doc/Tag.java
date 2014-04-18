fe2e38e55085e4fb703d19cf7f47ee7cfe37bad1
Igor Shevchenko <igoro.shevchenko@gmail.com>
2014-04-18 14:02:39 +0200

    Client-Server Link Repair.

--- src/main/java/it4bi/ufrt/ir/service/doc/Tag.java
+++ src/main/java/it4bi/ufrt/ir/service/doc/Tag.java
@@ -1,20 +1,32 @@
 package it4bi.ufrt.ir.service.doc;
 
 public class Tag {
-	public int getTagId() {
-		return tagId;
-	}
-
-	public String getTag() {
-		return tag;
-	}
 
 	public int tagId;
 	static public int tagIdCounter = 10000;
 	public String tag;
 	
+	public Tag() {
+	}
+	
 	public Tag(String tag) {
 		this.tag = tag;
 		tagId = tagIdCounter++;
+	}	
+	
+	public int getTagId() {
+		return tagId;
+	}
+	
+	public void setTagId(int tagId) {
+		this.tagId = tagId;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+	
+	public void setTag(String tag) {
+		this.tag = tag;
 	}
 }
