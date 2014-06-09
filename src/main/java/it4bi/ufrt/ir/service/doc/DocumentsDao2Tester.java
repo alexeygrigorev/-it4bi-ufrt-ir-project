96d316e8f3a2f088e6814c95bb797107f183c315
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2014-06-09 22:01:36 +0200

    removed unused junit import

--- src/main/java/it4bi/ufrt/ir/service/doc/DocumentsDao2Tester.java
+++ src/main/java/it4bi/ufrt/ir/service/doc/DocumentsDao2Tester.java
@@ -1,19 +1,11 @@
 package it4bi.ufrt.ir.service.doc;
 
-import static org.junit.Assert.assertEquals;
-import it4bi.ufrt.ir.service.users.User;
 import it4bi.ufrt.ir.service.users.UsersDAO;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -22,38 +14,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class DocumentsDao2Tester {
 
-	
 	public static void main(String[] args) throws SQLException {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("dataSources.xml");
-	 
-	        DocumentsDao docsDAO = new DocumentsDao( (NamedParameterJdbcTemplate) context.getBean("appJdbcTemplate"));
-	        UsersDAO usersDAO = new UsersDAO( (NamedParameterJdbcTemplate) context.getBean("appJdbcTemplate"));
-	        
-	        /*for(User user : UserDatabase.getUsers()) {
-	        	usersDAO.insertUser(user);
-	        }*/
-	        
-	        
-	        List<Tag> tags = new ArrayList<Tag>();
-	        tags.add(new Tag("Aa"));
-	        tags.add(new Tag("Bb"));
-	        
-	        DocumentRecord docRec = new DocumentRecord("title", 134, "app/pdf");
-	        docRec.setTags(tags);
-	        docRec.setDocPath("path");
-	        
-	        docsDAO.insertDocumentRecord(docRec);
-	        
-	        docsDAO.insertUserDocAssociation(7656, 24543, DOCUSER_ASSOC_TYPE.OWNS);
-	        docsDAO.insertUserDocAssociation(1826, 13543, DOCUSER_ASSOC_TYPE.LIKES);
-	        
-	        //docsDAO.updateUserTagScore(3826, 34, 2.4f);
-	        //docsDAO.updateUserTagScore(3826, 34, -0.2f);
-	        
-	        
-	        
-	        
-		
+
+		DocumentsDao docsDAO = new DocumentsDao(
+				(NamedParameterJdbcTemplate) context.getBean("appJdbcTemplate"));
+		UsersDAO usersDAO = new UsersDAO((NamedParameterJdbcTemplate) context.getBean("appJdbcTemplate"));
+
+		List<Tag> tags = new ArrayList<Tag>();
+		tags.add(new Tag("Aa"));
+		tags.add(new Tag("Bb"));
+
+		DocumentRecord docRec = new DocumentRecord("title", 134, "app/pdf");
+		docRec.setTags(tags);
+		docRec.setDocPath("path");
+
+		docsDAO.insertDocumentRecord(docRec);
+
+		docsDAO.insertUserDocAssociation(7656, 24543, DOCUSER_ASSOC_TYPE.OWNS);
+		docsDAO.insertUserDocAssociation(1826, 13543, DOCUSER_ASSOC_TYPE.LIKES);
+
 	}
 }
